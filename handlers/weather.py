837611f3c123908b624fec83d8e294d8091e95f3
Ayesha Cyril <ayeshacy@si-b7.nsk.21-school.ru>
2026-05-01 22:24:07 +0700

    upd handlers, services, utils

--- handlers/weather.py
+++ handlers/weather.py
@@ -1,34 +1,43 @@
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes, ConversationHandler
+
 from services.weather_service import get_weather
 from utils.logger.weather_logger import log_weather
 from utils.keyboards import back_button
 from states import WAIT_CITY
 
+
 async def ask_city(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.callback_query.message.reply_text("Введите город:")
-    return 1
+
+    query = update.callback_query
+    await query.answer()
+
+    await query.message.reply_text("🌍 Введите название города:")
+    return WAIT_CITY
+
 
 async def show_weather(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    city = update.message.text
-    data = await get_weather(city)
 
+    city = update.message.text.strip()
     user = update.effective_user
+
+    data = await get_weather(city)
 
     if not data:
         log_weather(user.id, user.username, city, "error")
-        await update.message.reply_text("Город не найден")
-        return 1
+        await update.message.reply_text("❌ Город не найден. Попробуйте ещё раз:")
+        return WAIT_CITY
 
     log_weather(user.id, user.username, city, "success")
 
     text = (
-        f"🌤️ {city}\n"
-        f"{data['weather'][0]['description']}\n"
-        f"🌡 {data['main']['temp']}°C\n"
-        f"💧 {data['main']['humidity']}%\n"
-        f"🌬 {data['wind']['speed']} м/с"
+        f"🌤 Погода: {city}\n"
+        f"📝 {data['weather'][0]['description']}\n"
+        f"🌡 Температура: {data['main']['temp']}°C\n"
+        f"💧 Влажность: {data['main']['humidity']}%\n"
+        f"🌬 Ветер: {data['wind']['speed']} м/с"
     )
 
     await update.message.reply_text(text, reply_markup=back_button())
-    return -1
+
+    return ConversationHandler.END
